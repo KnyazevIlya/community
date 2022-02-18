5374fe2204be5b2e12fa48d480b0de6e68f6c01d
knyazevilya <kknyaz033@gmail.com>
2022-02-18 17:53:47 +0200

    Initial Commit

---
+++ community/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  community
+//
+//  Created by Illia Kniaziev on 18.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
