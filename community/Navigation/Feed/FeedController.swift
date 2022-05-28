c1d6d441bb2c8fee649807f610eb4650824c65f4
Anatoliy <tolxpams@gmail.com>
2022-05-28 18:21:21 +0300

    open view report vc when cell did select

--- community/Navigation/Feed/FeedController.swift
+++ community/Navigation/Feed/FeedController.swift
@@ -8,9 +8,11 @@
 import UIKit
 import RxSwift
 
-class FeedController: UIViewController, UITableViewDelegate {
+class FeedController: UIViewController {
 
     @IBOutlet weak var pinsTable: UITableView!
+    
+    private var pins = [Int:Pin]()
     
     private let itemOffset: CGFloat = 5
     private let disposeBag = DisposeBag()
@@ -25,10 +27,21 @@ class FeedController: UIViewController, UITableViewDelegate {
         StorageManager.shared.pins.bind(to: pinsTable.rx.items) { cell, index, pin in
             let cell = cell.dequeueReusableCell(withIdentifier: PinInFeedCell.cellId,for: IndexPath(item: index, section: 0)) as! PinInFeedCell
             cell.uploadData(pin)
+            self.pins[index] = pin
             return cell
         }.disposed(by: disposeBag)
     }
-    
-    
 
+}
+
+extension FeedController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let pin = pins[indexPath.row] {
+            let viewModel = ViewReportViewModel(pin: pin)
+            let controller = ViewReportController(viewModel: viewModel)
+            present(controller, animated: true)
+        }
+    }
+    
 }
